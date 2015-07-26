aed2954e30013cb5c7656e3b3179ddc462972487
Abhishek Bajpai <abhisheietk@gmail.com>
2015-07-26 10:41:18 +0530

    update

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 #from distutils.core import setup
-from Cython.Build import cythonize
+#from Cython.Build import cythonize
 #from distutils.extension import Extension
 from distutils.core import setup, Extension
 
-'''C_module = Extension('crypto-g.libcipher',
-                     sources = ['crypto-g/cipher.c'],
-                     libraries=[])'''
+#'''C_module = Extension('crypto-g.libcipher',
+#                     sources = ['crypto-g/cipher.c'],
+#                     libraries=[])'''
 
-cython_modules = cythonize([Extension("gcipher", ["crypto-g1/gcipher.pyx"],
-                                      libraries=['cipher'],
-                                      #include_dirs=[ 'crypto-g' ],
-                                      #library_dirs=[ 'crypto-g' ],
-                                      ),
-        ])
+#cython_modules = cythonize([Extension("gcipher", ["crypto-g1/gcipher.pyx"],
+#                                      libraries=['cipher'],
+#                                      #include_dirs=[ 'crypto-g' ],
+#                                      #library_dirs=[ 'crypto-g' ],
+#                                      ),
+#        ])
 
-#modules = [C_module, cython_modules[0]]
-modules = [cython_modules[0]]
-#cython_modules.append(C_module)
+##modules = [C_module, cython_modules[0]]
+#modules = [cython_modules[0]]
+##cython_modules.append(C_module)
 
-setup(name='crypto-g1',
+setup(name='crypto_chaotic',
       version='0.1.0',
       author='Abhishek Bajpai',
       author_email='abbajpai@barc.gov.in',
-      packages=['crypto-g1'],
-      ext_modules = modules,
+      packages=['crypto_chaotic'],
+      #ext_modules = modules,
       #ext_modules = cythonize("PyPci/*.pyx"),
       scripts=[ ],
       url=' ',
